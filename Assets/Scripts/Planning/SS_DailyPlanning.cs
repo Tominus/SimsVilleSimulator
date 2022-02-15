9adcc6d5705e02d00695d1cfdb77ae9d5b08b6f0
Tominus <Tominusstarwars@gmail.com>
2022-02-15 14:46:44 +0100

    5

---
+++ Assets/Scripts/Planning/SS_DailyPlanning.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class SS_DailyPlanning
+{
+    
+}
--- Assets/Scripts/World/SS_World.cs
+++ Assets/Scripts/World/SS_World.cs
@@ -7,5 +7,23 @@ public class SS_World : SS_Singleton<SS_World>
     //[SerializeField] SS_PlanningManager planningManager = null;
     //[SerializeField] SS_BuildingManager buildingManager = null;
 
+    public bool IsValid => false;
 
+    private void Start()
+    {
+        InitSub();
+    }
+
+    void InitSub()
+    {
+        if (!IsValid) return;
+        //planningManager.OnEnd += SetPlanningManager;
+    }
+
+    /*
+    void SetPlanningManager(SS_PlanningManager _pm)
+    {
+        planningManager = _pm;
+    }
+     */
 }
