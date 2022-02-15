92d8f250f29b452a5fd08722ed97b01042281344
Tominus <Tominusstarwars@gmail.com>
2022-02-15 15:05:23 +0100

    5

--- Assets/Scripts/GameUI/SS_GameUI.cs
+++ Assets/Scripts/GameUI/SS_GameUI.cs
@@ -1,6 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SS_GameUI : MonoBehaviour
 {
-    
+    public event Action<float> OnWorldSpeedChange = null;
+
+    [SerializeField] Slider gameSpeedSlider = null;
+    [SerializeField] float baseWorldSpeed = 1, worldSpeedMultiplyer = 10;
+
+    private void Start()
+    {
+        Init();
+    }
+    private void OnDestroy()
+    {
+        OnWorldSpeedChange = null;
+    }
+
+    void Init()
+    {
+        gameSpeedSlider.onValueChanged.AddListener((call) => 
+        OnWorldSpeedChange?.Invoke(gameSpeedSlider.value * worldSpeedMultiplyer + baseWorldSpeed)
+        );
+    }
 }
--- Assets/Scripts/World/SS_World.cs
+++ Assets/Scripts/World/SS_World.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class SS_World : SS_Singleton<SS_World>
 {
-    //[SerializeField] SS_GameUI gameUI = null;
+    [SerializeField] SS_GameUI gameUI = null;
     //[SerializeField] SS_DayCycle dayCycle = null;
     //[SerializeField] SS_PlanningManager planningManager = null;
     //[SerializeField] SS_BuildingManager buildingManager = null;
 
-    public bool IsValid => false;
+    public bool IsValid => gameUI;
 
     private void Start()
     {
